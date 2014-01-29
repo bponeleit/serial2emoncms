e3640ebc853564288086dee1c244ac120e71d52f
Burkart Poneleit <burkart.poneleit@gmail.com>
2014-01-29 21:05:18 +0100

    works with ISKRA MT174

--- UsbIRHead.py
+++ UsbIRHead.py
@@ -1,30 +1,32 @@
-from serial import Serial
+import serial
+import time
+ser = serial.Serial()
+ser.port = '/dev/ttyUSB0'
+ser.baudrate = 300
+ser.parity = serial.PARITY_EVEN
+ser.stopbits = serial.STOPBITS_ONE
+ser.bytesize = serial.SEVENBITS
+ser.timeout = 1
 
-ser = serial.Serial(
-    port='/dev/ttyUSB0',
-    baudrate=300,
-    parity=serial.PARITY_EVEN,
-    stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.SEVENBITS
-)
-
-try: 
+try:
  ser.open()
 except Exception, e:
  print "error open serial port: " + str(e)
  exit()
- 
-wait(500)
+
+time.sleep(0.5)
 
 if ser.isOpen():
- try:
+# try:
   ser.write("/?!\r\n")
   while True:
    response = ser.readline()
    print response
-   ser.close()
- except Exception, e1:
-	    print "error communicating...: " + str(e1)
+  ser.close()
+  
+#  except Exception, e1:
+#   print "error communicating...: " + str(e)
+
 else:
-    print "cannot open serial port "
+ print "cannot open serial port"
 
