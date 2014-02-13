704fe4b9483eb655b64af6582fb3018176dab297
Burkart Poneleit <burkart.poneleit@gmail.com>
2014-02-13 21:19:58 +0100

    use oemgateway as basis

--- UsbIRHead.py
+++ UsbIRHead.py
@@ -1,5 +1,10 @@
-from serial import Serial
+"""
+Use oemgateway with IEC 62056-21 compatible smart meter
+"""
+import serial
+from oemgateway.oemgatewaylistener import OemGatewaySerialListener
 
+# Open serial port
 ser = serial.Serial(
     port='/dev/ttyUSB0',
     baudrate=300,
@@ -8,23 +13,56 @@ ser = serial.Serial(
     bytesize=serial.SEVENBITS
 )
 
-try: 
- ser.open()
+try:
+    ser.open()
 except Exception, e:
- print "error open serial port: " + str(e)
- exit()
- 
+    print "error open serial port: " + str(e)
+    exit()
+
 wait(500)
 
 if ser.isOpen():
- try:
-  ser.write("/?!\r\n")
-  while True:
-   response = ser.readline()
-   print response
-   ser.close()
- except Exception, e1:
-	    print "error communicating...: " + str(e1)
+    try:
+        ser.write("/?!\r\n")
+        while True:
+            response = ser.readline()
+            print response
+            ser.close()
+    except Exception, e1:
+        print "error communicating...: " + str(e1)
 else:
     print "cannot open serial port "
 
+class OemGatewaySmartMeterListener(OemGatewaySerialListener):
+    """class OemGatewaySmartMeterListener
+
+    Reads data from smart meter on serial port
+
+    """
+    def __init__(self, com_port, option_select):
+        """
+        Initialize listener
+        """
+        super(OemGatewaySmartMeterListener, self).__init__(com_port)
+        self._request_message = "/?!\r\n"
+        self._option_select = option_select
+        self._id = None
+
+    def read(self):
+        """
+        read data set
+        """
+        ser._rx_buf = ""
+        self._ser.write(self._request_message)
+
+        self._id = self._ser.readline().rstrip
+
+        self._ser.write(self._option_select)
+
+        # Read serial RX
+        while "\03" not in self._rx_buf:
+            self._rx_buf = self._rx_buf + self._ser.read()
+
+        self._ser.read()
+
+#class OemGatewayIskraMT174Listener(OemGatewaySmartMeterListener):
